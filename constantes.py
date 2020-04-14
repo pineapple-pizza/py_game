09d368c000f899cf907b3f25f443a72cb7532565
pineapple-pizza <an.aslanian@gmail.com>
2020-04-14 18:01:53 +0200

    adding const

---
+++ constantes.py
@@ -0,0 +1,18 @@
+import pygame
+
+WIN_TITLE = "MacGyver : The Maze Runner"
+
+#CHARACTERS
+ICON_MACGYVER = "ressource/MacGyver.png"
+ICON_GUARDIAN = "ressource/Gardien.png"
+
+#MAZE
+WALL = "ressource/wall.png"
+FLOOR = "ressource/floor.png"
+
+#WINDOW PARAM
+SIDE_SPRITE_NUM = 15
+SPRITE_SIZE = 30
+SIDE_WINDOW = SIDE_SPRITE_NUM * SPRITE_SIZE
+
+HOME_PAGE = "ressource/home.png"
